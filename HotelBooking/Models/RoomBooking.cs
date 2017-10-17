10181eaf4cf07349cb8779a180786f4d41e7eb1a
Theotricem <theotricem@yahoo.com>
2017-10-17 12:55:09 -0400

    Created RoomDetails Class and RoomBooking Class

---
+++ HotelBooking/Models/RoomBooking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelBooking.Models
+{
+    public class RoomBooking
+    {
+        public int RoomBookingId { get; set; }
+        public int RoomId { get; set; }
+        public string BookedTo { get; set; }
+        public string BookedFrom { get; set; }
+        public string BookingStatus { get; set; }
+        public string AdvancePayed { get; set; }
+        public string TotalAmountPayed { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
---
+++ HotelBooking/Models/RoomDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelBooking.Models
+{
+    public class RoomDetails
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public string RoomPrice { get; set; }
+    }
+}
